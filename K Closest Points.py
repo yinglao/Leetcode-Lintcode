b7b3397999fd31a66c396687d3b71e547160ac92
Yinglao Liu <yl3596@columbia.edu>
2018-02-18 18:11:46 -0800

    Update K Closest Points.py

--- K Closest Points.py
+++ K Closest Points.py
@@ -5,10 +5,13 @@
 #Example
 #Given points = [[4,6],[4,7],[4,4],[2,5],[1,1]], origin = [0, 0], k = 3
 #return [[1,1],[2,5],[4,4]]
+#http://www.lintcode.com/en/problem/k-closest-points/
+
 
 #建立一个含有k个元素的deque，每个元素是[distance, x, y]， 一开始先将前k个点加进去，后面每加进去一个点的信息，就进行一次排序，再根据排序完的结果,去掉最后一名，如此直至所有点遍历完。
 #T(n) = O(nklog(k))
 #可以优化成O(nk)
+
 
 
 
